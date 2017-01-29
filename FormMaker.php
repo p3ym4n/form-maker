5d6f9eb0a9f285d89156b62b2630cc9a52e1cb10
p3ym4n <peyman.goldaste@gmail.com>
2017-01-30 00:43:58 +0330

    fix carbon or string date.

--- FormMaker.php
+++ FormMaker.php
@@ -15,7 +15,7 @@ use App\Http\Assets\SortableAsset;
 use App\Http\Assets\TagsInputAsset;
 use Asset;
 use Illuminate\Database\Eloquent\Model;
-use p3ym4n\JDte\JDate;
+use p3ym4n\JDate\JDate;
 use SplFileInfo;
 
 /**
